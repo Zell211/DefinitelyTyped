30ac4fb157a3ab25d2d6e43301a8e3ad04fe3298
Zell211 <audibert.ludovic@gmail.com>
2018-05-01 11:26:08 +0200

    Update index.d.ts
    Update for 1.0.0

--- types/require-dir/index.d.ts
+++ types/require-dir/index.d.ts
@@ -1,8 +1,16 @@
-// Type definitions for require-dir 0.3
+// Type definitions for require-dir 1.0.0
 // Project: https://github.com/aseemk/requireDir
 // Definitions by: weekens <https://github.com/weekens>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare function requireDir(directory: string): { [path: string]: any };
+declare function requireDir(
+  directory: string, 
+  options: {
+    recurse: boolean,
+    filter: Function,
+    mapKey: Function,
+    mapValue: Function,
+    duplicates: boolean,
+  }): { [path: string]: any };
 
 export = requireDir;
